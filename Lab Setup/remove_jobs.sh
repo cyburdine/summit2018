56658885a04b79cd9633ad6bcf8b1ba1b3853c4b
Justin Burdine <justin@cyburdine.com>
2018-05-02 09:04:51 -0500

    removing hard coded tower ips and moving them to tower inventory

---
+++ Lab Setup/remove_jobs.sh
@@ -0,0 +1,12 @@
+### this script cleans up all the extraneous jobs that are on the master tower template
+
+tower-cli job_template delete 88
+tower-cli job_template delete 81
+tower-cli job_template delete 82
+tower-cli job_template delete 105
+tower-cli job_template delete 106
+tower-cli job_template delete 107
+tower-cli job_template delete 108
+tower-cli job_template delete 109
+tower-cli job_template delete 110
+tower-cli job_template delete 87
